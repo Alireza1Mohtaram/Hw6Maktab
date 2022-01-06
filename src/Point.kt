78c3ad32a7a8a3fabda9270ea367c147995d58ce
Alireza Mohtaram <Alireza.Pro8106@gmail.com>
2022-01-06 20:50:37 +0330

    Version 1.1

--- src/Point.kt
+++ src/Point.kt
@@ -2,7 +2,6 @@ import kotlin.collections.ArrayList
 import kotlin.math.max
 
 class Matrix() {
-
     var x: Int = 1
         set(value) {
             if (value <= 1) field = 1
@@ -18,70 +17,44 @@ class Matrix() {
     init {
         creatMatrix()
     }
-
     constructor(x: Int, y: Int) : this() {
         if (x >= 1) {
             this.x = x
         } else {
             this.x = 1
         }
-
         this.y = y
         creatMatrix()
-
-
     }
-
-
     fun setElement(x: Int, y: Int, value: Int) {
-
-        if (x > this.x || y > this.y) {
-            println("This opreation is not executable ")
-        } else {
-
-            this.matrix[x][y] = value
-        }
+        if (x > this.x || y > this.y) println("This opreation is not executable ")
+         else this.matrix[x][y] = value
 
     }
 
     fun getElement(x: Int, y: Int): Int {
         return this.matrix[x][y]
     }
-
-
     fun creatMatrix() {
-
         matrix = Array(x) { IntArray(y) }
-
     }
-
     override fun toString(): String {
-
-
         var str: StringBuilder = StringBuilder()
         for (i in 0 until x) {
-
             for (j in 0 until y) {
                 str.append("${matrix[i][j]}  ")
             }
             str.append("\n")
         }
-
         return str.toString()
     }
-
-
 
     private fun copyMatrix(m: Matrix): Array<IntArray> {
 
         var matrixCopy = Array(m.x) { IntArray(m.y) }
-
         for (i in 0 until x) {
             for (j in 0 until y) {
-
                 matrixCopy[i][j] = this.getElement(i, j)
-
-
             }
         }
         return matrixCopy
